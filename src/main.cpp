1d453c6332c9251d53ca5c846f4a5fc8d646b76e
quantumbit1 <kshitizyaksh@gmail.com>
2026-05-28 19:17:05 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-
-  std::cout << "$ ";
   std::string command;
-  std::cin >> command;
+  std::cout << "$ ";
+  std::getline(std::cin, command);
   std::cout << command <<": command not found" << std::endl;
 }
