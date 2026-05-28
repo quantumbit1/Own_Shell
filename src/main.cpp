bc70596665e49110448595674a2cf61f9797e20d
quantumbit1 <kshitizyaksh@gmail.com>
2026-05-28 19:03:50 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  std::string command;
   std::cout << "$ ";
+  std::cin >> command;
+  std::cout << command<<": command not found" << std::endl;
 }
