19c4fd96f69408dccd447a713dc7995d3cbe7d80
quantumbit1 <kshitizyaksh@gmail.com>
2026-05-28 19:59:35 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,24 @@ int main() {
     if(command == "exit"){
       break;
     }
-    else if(command.substr(0,4) == "echo"){
+    else if(command.substr(0,4) == "echo "){
       std::cout << command.substr(5) << std::endl;
+    }
+    else if(command.substr(0,4) == "type "){
+      std::string command_to_be_checked;
+      command_to_be_checked = command.substr(command.substr(5).find(" "));
+      if(command_to_be_checked == "echo"){
+        std::cout << "echo is a built-in command" << std::endl;
+      }
+      else if(command_to_be_checked == "type"){
+        std::cout << "type is a built-in command" << std::endl;
+      }
+      else if(command_to_be_checked == "exit"){
+        std::cout << "exit is a built-in command" << std::endl;
+      }
+      else{
+        std::cout << command_to_be_checked << "invalid_command: not found" << std::endl;
+      }
     }
     else{
       std::cout << command << ": command not found" << std::endl;
